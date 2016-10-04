e2e7b5df37f3868ee84346d2bf9f4d49712a2c5d
Etienne2911 <chevrieretienne22@gmail.com>
2016-10-04 02:00:06 +0200

    deck

--- Creature.java
+++ Creature.java
@@ -1,27 +1,22 @@
 package edu.insightr.spellmonger;
 
-/**
- * Created by Karim_Utilisateur on 24/09/2016.
- */
 public class Creature {
-    public final int damage = 0;
-    private int attackEagle, attackWolf, attackBear;
-    public Creature()
-    {
-        this.attackEagle = damage - 1;
-        this.attackBear = damage - 3;
-        this.attackWolf = damage - 2;
+
+    private int damage;
+    private String animal;
+
+    public Creature(String animal) {
+        this.animal = animal;
+        if (animal == "Eagle") {
+            this.damage = 1;
+        } else if (animal == "Wolf") {
+            this.damage = 2;
+        } else if (animal == "Bear") {
+            this.damage = 3;
+        }
     }
 
-    public int getAttackBear(){
-        return attackBear;
-    }
-
-    public int getAttackEagle(){
-        return attackEagle;
-    }
-
-    public int getAttackWolf(){
-        return attackWolf;
+    public int getDamage() {
+        return damage;
     }
 }
