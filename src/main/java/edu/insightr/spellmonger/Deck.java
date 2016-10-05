704de9170bb11a34b3a83467cfd548d2dff5b00e
Etienne <chevrieretienne22@gmail.com>
2016-10-05 14:50:04 +0200

    ajout de decks

--- src/main/java/edu/insightr/spellmonger/Deck.java
+++ src/main/java/edu/insightr/spellmonger/Deck.java
@@ -8,8 +8,8 @@ import java.util.Random;
  * Created by Etienne on 03/10/2016.
  */
 public class Deck {
-    List<String> cardPool = new ArrayList<>(70);
-    List<String> discard = new ArrayList<>(70);
+    List<Card> cardPool = new ArrayList<>(70);
+    List<Card> discard = new ArrayList<>(70);
     static Random rand = new Random();
     private int nbRand=0;
 
@@ -19,55 +19,86 @@ public class Deck {
     }
 
     //initialisation aléatoire du deck
-    public List<String> InitDeck()
+    public List<Card> InitDeck()
     {
+        Card carte;
         for (int i = 0; i < 70; i++)
         {
             nbRand=rand.nextInt(6 - 1 + 1) +1;
             if(nbRand==1)
             {
-                cardPool.add("Ritual");
+                carte=new Card("Ritual");
+                cardPool.add(carte);
             }
             else
             {
-                cardPool.add("Creature");
+                carte=new Card("Creature");
+                cardPool.add(carte);
             }
         }
 
         return cardPool;
     }
 
-    public String drawCard(int currentCardNumber,List<String> deck)
+    public void AjouterCard(Card carte)
     {
-        String card="";
-        if ("Creature".equalsIgnoreCase(deck.get(currentCardNumber))) {
+        discard.add(carte);
+    }
+
+    public void RetirerCard(Card carte)
+    {
+        discard.remove(carte);
+    }
+
+    //retourne objet carte contenant son nom, son type et ses dégâts
+    public Card drawCard(int currentCardNumber,List<Card> deck)
+    {
+
+        Card carte=new Card("");
+
+        if("Creature".equalsIgnoreCase(deck.get(currentCardNumber).getType()))
+        {
+            carte=new Card("Creature");
+        }
+        else
+        {
+            carte=new Card("Ritual");
+        }
+        return carte;
+
+        //String card="";
+        /*if ("Creature".equalsIgnoreCase(deck.get(currentCardNumber))) {
             Random rand = new Random();
             int randNb = rand.nextInt(3);
             if (randNb == 0){
-                card="Eagle";
-                discard.add(card);
+                Creature eagle = new Creature("Eagle");
+                carte=new Card(eagle.getName(),"Creature",eagle.getDamage());
+                //discard.add(card);
             }
             else if (randNb == 1){
-                card="Wolf";
-                discard.add(card);
+                Creature wolf = new Creature("Wolf");
+                carte=new Card(wolf.getName(),"Creature",wolf.getDamage());
+                //discard.add(card);
             }
             else if(randNb == 2){
-                card="Bear";
-                discard.add(card);
+                Creature bear = new Creature("Bear");
+                carte=new Card(bear.getName(),"Creature",bear.getDamage());
+                //discard.add(card);
             }
         }
         else if("Ritual".equalsIgnoreCase(deck.get(currentCardNumber))) {
             Random rand = new Random();
             int randNb = rand.nextInt(2);
             if (randNb == 0) {
-                card="Curse";
-                discard.add(card);
+                Ritual curse = new Ritual("Curse");
+                carte=new Card(curse.getName(),"Ritual",curse.getDamage());
+                //discard.add(card);
             }
             else if(randNb == 1) {
-                card="Blessing";
-                discard.add(card);
+                Ritual blessing = new Ritual("Blessing");
+                carte=new Card(blessing.getName(),"Ritual",blessing.getDamage());
+                //discard.add(card);
             }
-        }
-        return card;
+        }*/
     }
 }
