166da7364027b3aeed4f953f8e4df774ecaba133
Wahhlid <err.walid@gmail.com>
2016-10-05 15:23:02 +0200

    creature test + app test

--- src/main/java/edu/insightr/spellmonger/Creature.java
+++ src/main/java/edu/insightr/spellmonger/Creature.java
@@ -3,7 +3,7 @@ package edu.insightr.spellmonger;
 /**
  * Created by Walidey on 21/09/2016.
  */
-public class Creature {
+public class Creature extends Card {
 
     private int damage;
     private String capacity;
@@ -11,20 +11,26 @@ public class Creature {
     private int PV;
 
 
-    public Creature(String animal) {
+
+    public Creature(String animal,String name,String type ) {
+        super( name, type);
         this.animal = animal;
+        type="creature";
         if (animal == "Eagle") {
             this.damage = 1;
             this.capacity = "Flying";
             this.PV = 1;
+            name = "Eagle";
         } else if (animal == "Wolf") {
             this.damage = 2;
             this.PV=2;
             this.capacity = "None";
+            name="Wolf";
         } else if (animal == "Bear") {
             this.damage = 3;
             this.PV = 3;
             this.capacity = "None";
+            name="Bear";
         }
     }
 
@@ -36,7 +42,8 @@ public class Creature {
     public String getCapacity() {return capacity;}
 
     public boolean IsAlive()    {
-return false;
+
+    return false;
 
     }
 
