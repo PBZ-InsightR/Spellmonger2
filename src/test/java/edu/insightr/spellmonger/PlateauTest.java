dbe441539e1bc14b380cb963a65d532301aab4ba
V-Boccia <boccia.valentin@gmail.com>
2016-10-10 14:27:37 +0200

    Création du fichier de test pour la classe plateau

---
+++ src/test/java/edu/insightr/spellmonger/PlateauTest.java
@@ -0,0 +1,36 @@
+package edu.insightr.spellmonger;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by meo-v on 10/10/2016.
+ */
+public class PlateauTest {
+    @Test
+    public void getNbTours() throws Exception {
+
+    }
+
+    @Test
+    public void getP1() throws Exception {
+
+    }
+
+    @Test
+    public void getP2() throws Exception {
+
+    }
+
+    @Test
+    public void getGagnant() throws Exception {
+
+    }
+
+    @Test
+    public void calculDegat() throws Exception {
+
+    }
+
+}
