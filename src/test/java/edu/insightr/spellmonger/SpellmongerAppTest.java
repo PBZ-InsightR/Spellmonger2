88eae12f1431617f11d0ac6fd5f763e83f6e8425
V-Boccia <boccia.valentin@gmail.com>
2016-10-12 16:19:42 +0200

    creation class test de spellmogerAppTest

---
+++ src/test/java/edu/insightr/spellmonger/SpellmongerAppTest.java
@@ -0,0 +1,16 @@
+package edu.insightr.spellmonger;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by meo-v on 10/12/2016.
+ */
+public class SpellmongerAppTest {
+    @Test
+    public void main() throws Exception {
+
+    }
+
+}
