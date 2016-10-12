ec5d9dc06401e20a74bbaac24237f7516447df61
V-Boccia <boccia.valentin@gmail.com>
2016-10-12 15:59:44 +0200

    Modification classTest Deck

--- src/test/java/edu/insightr/spellmonger/DeckTest.java
+++ src/test/java/edu/insightr/spellmonger/DeckTest.java
@@ -16,4 +16,27 @@ public class DeckTest {
         Assert.assertEquals(40,d.getCardPool().size());
         
     }
+    @Test
+    public void AjouterCard() throws Exception {
+       Deck d = new Deck(40);
+        Deck res = new Deck(41);
+        Creature creat1 = new Creature("Eagles");
+        Card c1 = new Card(creat1 );
+
+        //assertEquals(d.InitDeck().size() + 1, d.InitDeck().size());
+        //assertTrue("la carte à bien été ajouté", d.AjouterCard(c1) = );
+        assertEquals(d.InitDeck().size(),res.InitDeck().size());
+    }
+
+    @Test
+    public void RetirerCard() throws Exception {
+
+
+    }
+    @Test
+    public void drawCard() throws Exception {
+
+
+    }
+
 }
