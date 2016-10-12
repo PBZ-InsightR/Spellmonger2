5e77a5d496b77db6035999e74f4ae8b07c18e010
Karim_Utilisateur <karim.benmoussa33@gmail.com>
2016-10-12 15:30:09 +0200

    Classe Plateau

--- src/test/java/edu/insightr/spellmonger/CardTest.java
+++ src/test/java/edu/insightr/spellmonger/CardTest.java
@@ -6,6 +6,7 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class CardTest {
+    
     @Test
     public void isCreature() throws Exception {
         Creature wolf = new Creature("Wolf");
