c8de5a67bacfa0396ec7454e78aef48f0ecaf620
TheoBiron <yami-theo@hotmail.fr>
2016-10-16 17:29:34 +0200

    Amélioration

--- src/main/java/edu/insightr/spellmonger/Game.java
+++ src/main/java/edu/insightr/spellmonger/Game.java
@@ -17,13 +17,13 @@ public class Game extends Application {
 
     @Override
     public void start(Stage primaryStage) {
-        try{
+        try {
             final URL url = getClass().getResource("/Gamefxml.fxml");
             final FXMLLoader fxmlLoader = new FXMLLoader(url);
             final AnchorPane root = (AnchorPane) fxmlLoader.load();
-            final Scene scene = new Scene(root, 1200,600);
+            final Scene scene = new Scene(root, 1200, 600);
             primaryStage.setScene(scene);
-        }catch (IOException ex){
+        } catch (IOException ex) {
             System.err.println("Erreur" + ex);
         }
         primaryStage.setTitle("Card Game");
@@ -38,27 +38,28 @@ public class Game extends Application {
 
     @FXML
     private Label hpPlayer1, hpPlayer2, nomPlayer1, nomPlayer2;
-    private Plateau plateau = new Plateau("Alice", "Bob", 20, 0);
-    private Player player1 = new Player("Alice", 20, 0);
-    private Player player2 = new Player("Bob", 20, 0);
+    @FXML
     private Button buttonPlayer1, buttonPlayer2;
 
+    private Plateau plateau = new Plateau("Alice", "Bob", 20, 0);
+
+
     @FXML
-    private void drawPlayer2(){
+    private void drawPlayer1() {
         plateau.Jeu();
+        //hpPlayer1.setText(Integer.toString(plateau.getCurrent().GetLifePoints()));
+        nomPlayer1.setText("Alice");
+        buttonPlayer1.setDisable(true);
+        buttonPlayer2.setDisable(false);
+    }
+
+    @FXML
+    private void drawPlayer2() {
+        plateau.Jeu();
+        //hpPlayer1.setText(Integer.toString(plateau.getCurrent().GetLifePoints()));
         hpPlayer2.setText("10");
         nomPlayer2.setText("Bob");
-        /*buttonPlayer1.setDisable(false);
-        buttonPlayer2.setDisable(true);*/
+        buttonPlayer1.setDisable(false);
+        buttonPlayer2.setDisable(true);
     }
-
-    @FXML
-    private void drawPlayer1(){
-        plateau.Jeu();
-        hpPlayer1.setText("15");
-        nomPlayer1.setText("Alice");
-        /*buttonPlayer1.setDisable(true);
-        buttonPlayer2.setDisable(false);*/
-    }
-
 }
