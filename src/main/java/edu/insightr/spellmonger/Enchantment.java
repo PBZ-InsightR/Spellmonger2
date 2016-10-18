dcbd8e0b1e075929e90409b8015e287115e706b1
Dorian <do95250@hotmail.fr>
2016-10-18 13:57:57 +0200

    Class enchantment

---
+++ src/main/java/edu/insightr/spellmonger/Enchantment.java
@@ -0,0 +1,72 @@
+package edu.insightr.spellmonger;
+
+/**
+ * Created by Dorian on 18/10/2016.
+ */
+import java.util.Random;
+
+public class Enchantment extends Card {
+    private int energy;
+    private int energyCost;
+    private String enchantmentName;
+    public Enchantment(String enchantmentName)
+    {
+        super(enchantmentName);
+        this.enchantmentName=enchantmentName;
+        if (enchantmentName.equals("Vault overclocking"))
+        {
+            this.energy=1;
+            this.energyCost=3;
+        }
+    }
+    public boolean VaultOverburn()
+    {
+        int nbRand;
+        Random rand = new Random();
+        nbRand = rand.nextInt(3);
+        if(nbRand==0)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+
+    }
+    public String getName() {return enchantmentName;}
+    public int getEnergy() {
+        return energy;
+    }
+    public int getEnergyCost() {
+        return energyCost;
+    }
+    @Override
+    public int getDamage()
+    {
+        int damage =0;
+        if (enchantmentName.equals("Vault overclocking"))
+        {
+            damage = 0;
+        }
+        return damage;
+    }
+
+    public String toString() {
+        String message = "";
+        switch (enchantmentName) {
+            case "Vault overclocking":
+                if(VaultOverburn()==true)
+                {
+                    message = "Vault overburn and is empty";
+                    break;
+                }
+                else
+                {
+                    message = "L'enchantement fait " + getEnergy() + " points d'énergie";
+                    break;
+                }
+        }
+        return message;
+    }
+}
